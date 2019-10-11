2d75e59a0d42af5d272051323c856f872df46f19
josh <tailrin.jh@gmail.com>
2019-10-11 12:27:44 -0600

    added complete files

---
+++ script.js
@@ -0,0 +1,75 @@
+const apiKey = "77TcsfiQrsXysvXgRADWsmsQxNQuNjGSMbUZM5Ud";
+const baseUrl =  "https://developer.nps.gov/api/v1/parks";
+
+
+
+function createBox(state){
+  return `<div class="checkboxes"><input type="checkbox" name="${state.code}" value="${state.name}"> <label class="box-label" for='${state.name}'>${state.name}</label></div>`;
+}
+
+
+$('document').ready(function(){
+  $('#checkboxes').html(createCheckBoxes());
+});
+
+function createCheckBoxes(){
+  const arr = [];
+  states.forEach(state => {
+    arr.push(createBox(state))
+  });
+  return arr.join("");
+}
+
+ function displayResults(url){
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+      const arr =[];
+      console.log(responseJson);
+      responseJson.data.forEach(datum => {
+      const result = `<li class="result-item"><h3>${datum.fullName}</h3><p>${datum.description}</p><a href="${datum.url}">${datum.url}</a></li>`;
+      arr.push(result);
+      });
+     $("#result-list").html(arr.join(""));
+    });
+ }
+
+
+
+function getParks(Limit){
+  const params = {
+  api_key:apiKey,
+  limit: Limit,
+  statesCodes: []
+  }
+  $('input:checked').each(function () {
+  params.statesCodes.push($(this).attr('name'));
+  });
+  const url = baseUrl + '?' + formatParams(params);
+  console.log(url);
+  displayResults(url);
+}
+
+function formatParams (params) {
+  if (params.statesCodes.length !==0 ){
+    const stateCodeString = `stateCode=${params.statesCodes.join("&stateCode=")}`;
+    delete params.statesCodes;
+    const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&') + '&' + stateCodeString;
+  }
+  delete params.statesCodes;
+  const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function watchForm(){
+$('form').submit(event => {
+  event.preventDefault();
+  const limit = $(".limit").val();
+  getParks(limit);
+ });
+}
+
+$(watchForm());
